dabcf803ca58a95198a401e9faa874ff063d202d
shakibania <shakiba@gmail.com>
2019-09-22 15:53:26 +0330

    786

---
+++ www/js/index.js
@@ -0,0 +1,12 @@
+
+
+$( document ).ready(function() {
+	// var expireDateStr = "06/10/2019";//mm/dd/yyyy
+	// var expireDateArr = expireDateStr.split("/");
+	// var expireDate = new Date(expireDateArr[2], expireDateArr[0], expireDateArr[1]);
+	// var todayDate = new Date();
+	// if (todayDate > expireDate) {
+	   // $("body").html("");
+	   // $("body").css("background","#000000");
+	// };
+});
